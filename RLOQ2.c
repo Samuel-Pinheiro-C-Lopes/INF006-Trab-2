f2508a3d33a07afa49fec6e6bcecb49812752be7
Jansey Scofield <163556555+JanseyScofield@users.noreply.github.com>
2025-01-08 20:55:54 +0000

    Adicionando a struct da pilha. Tarefa Notion : Struct questão 2

--- RLOQ2.c
+++ RLOQ2.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct pilha Pilha;
 typedef struct itemPilha ItemPilha;
-typedef struct fila Fila;
 typedef struct itemFila ItemFila;
+
+typedef struct {
+    ItemPilha *topo;
+}Pilha;
 
 typedef struct{
     ItemPilha *abaixo;
